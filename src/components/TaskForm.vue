0415e4b35f154a8596a922b2de27f615fc5aff95
Jan-Niklas Reinhardt <reinhardtjanniklas96@gmail.com>
2025-06-03 17:58:18 +0200

    adds first components

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits<{ addTask: [newTask: string] }>();
+const newTask = ref("");
+
+const error = ref("");
+
+function addTask() {
+  if (newTask.value.trim()) {
+    emit("addTask", newTask.value);
+  } else {
+    error.value = "Please enter a valid task";
+  }
+  newTask.value = "";
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTask">
+    <label for="newTask">
+      New Task
+      <input
+        v-model="newTask"
+        type="text"
+        name="newTask"
+        id="newTask"
+        :aria-invalid="!!error || undefined"
+        @input="error = ''"
+      />
+      <small v-if="error" id="invalid-helper">{{ error }}</small>
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ tasks: Task[] }>();
+</script>
+
+<template>
+  <article v-for="task in props.tasks" :key="task.id">
+    <h3>{{ task.title }}</h3>
+  </article>
+</template>
